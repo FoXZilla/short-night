cfabcb67eb071d9014bcb13b05775adf8f9f51c5
Raihanul Islam Refat <refat0167@gmail.com>
2022-08-24 12:25:38 +0600

    fix lint: comma spacing

--- src/common/TimeSpliter.ts
+++ src/common/TimeSpliter.ts
@@ -32,44 +32,44 @@ export default class TimeSpliter {
         [methName in DateBy]: (date :Date, step? :number) => Date
     } = {
             fiveCentury(date :Date, step = 1) {
-                date.setMonth(0 , 1);
-                date.setHours(0 , 0 , 0 , 1);
+                date.setMonth(0, 1);
+                date.setHours(0, 0, 0, 1);
 
                 date.setFullYear(Math.floor(date.getFullYear() / 500) * 500 + (step * 100 * 5));
 
                 return date;
             },
             century(date :Date, step = 1) {
-                date.setMonth(0 , 1);
-                date.setHours(0 , 0 , 0 , 1);
+                date.setMonth(0, 1);
+                date.setHours(0, 0, 0, 1);
 
                 date.setFullYear(Math.floor(date.getFullYear() / 100) * 100 + (step * 100));
 
                 return date;
             },
             tenYear(date :Date, step = 1) {
-                date.setMonth(0 , 1);
-                date.setHours(0 , 0 , 0 , 1);
+                date.setMonth(0, 1);
+                date.setHours(0, 0, 0, 1);
 
                 date.setFullYear(Math.floor(date.getFullYear() / 10) * 10 + (step * 10));
 
                 return date;
             },
             year(date :Date, step = 1) {
-                date.setMonth(0 , 1);
-                date.setHours(0 , 0 , 0 , 1);
+                date.setMonth(0, 1);
+                date.setHours(0, 0, 0, 1);
 
                 date.setFullYear(date.getFullYear() + step);
 
                 return date;
             },
             quarter(date :Date) {
-                date.setMonth(~~(date.getMonth() / 3) * 3 , 1);
-                date.setHours(0 , 0 , 0 , 1);
+                date.setMonth(~~(date.getMonth() / 3) * 3, 1);
+                date.setHours(0, 0, 0, 1);
 
                 if (date.getMonth() + 3 >= 12) {
                     date.setFullYear(date.getFullYear() + 1);
-                    date.setMonth(0 , 1);
+                    date.setMonth(0, 1);
                 } else {
                     date.setMonth(date.getMonth() + 3, 1);
                 }
@@ -78,7 +78,7 @@ export default class TimeSpliter {
             },
             month(date :Date) {
                 date.setDate(1);
-                date.setHours(0 , 0 , 0 , 1);
+                date.setHours(0, 0, 0, 1);
 
                 if (date.getMonth() === 11) {
                     date.setFullYear(date.getFullYear() + 1);
@@ -90,7 +90,7 @@ export default class TimeSpliter {
                 return date;
             },
             week(date :Date) {
-                date.setHours(0 , 0 , 0 , 1);
+                date.setHours(0, 0, 0, 1);
 
                 do {
                     date.setTime(date.getTime() + 1000 * 60 * 60 * 24);
@@ -99,7 +99,7 @@ export default class TimeSpliter {
                 return date;
             },
             day(date :Date) {
-                date.setHours(0 , 0 , 0 , 1);
+                date.setHours(0, 0, 0, 1);
 
                 date.setTime(date.getTime() + 1000 * 60 * 60 * 24);
 
